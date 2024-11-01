ae8398c8209783e00d53b2bd1db7772dccd477e9
yamaguchi kohei <torokeruretoro@gmail.com>
2024-11-01 11:56:35 +0900

    プロジェクト作成

---
+++ qiita-test/View/ViewController/QiitaMainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  QiitaMainViewController.swift
+//  qiita-test
+//
+//  Created by yamaguchi kohei on 2024/11/01.
+//
+import UIKit
+import Foundation
+
+class QiitaMainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
